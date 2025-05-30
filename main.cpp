ac4dd44dd9a4c03b41c82c8042c4f02ea322a8fc
Petteri Niemi <petteri.niemi@vertex.fi>
2025-05-30 11:33:03 +0300

    Clean up main function

--- main.cpp
+++ main.cpp
@@ -6,34 +6,27 @@ static std::shared_ptr<Keyboard> KEYBOARD = std::make_shared<Keyboard>();
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-  HWND hWnd;
-  WNDCLASSEX wc;
-
-  ZeroMemory(&wc, sizeof(WNDCLASSEX));
-
-  wc.cbSize = sizeof(WNDCLASSEX);
+  WNDCLASSEX wc = { 0 };
+  wc.cbSize = sizeof(wc);
   wc.style = CS_HREDRAW | CS_VREDRAW;
   wc.lpfnWndProc = MessageProcessor;
   wc.hInstance = hInstance;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-  // wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
   wc.lpszClassName = L"WindowClass1";
-
   RegisterClassEx(&wc);
 
   RECT wr = { 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT };
   AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
 
-  hWnd = CreateWindowEx(NULL, L"WindowClass1", L"MinecraftProject", WS_OVERLAPPEDWINDOW, 300, 300, SCREEN_WIDTH, SCREEN_HEIGHT, NULL, NULL, hInstance, NULL);
+  HWND hWnd = CreateWindowEx(NULL, L"WindowClass1", L"MinecraftProject", WS_OVERLAPPEDWINDOW, 300, 300, SCREEN_WIDTH, SCREEN_HEIGHT, nullptr, nullptr, hInstance, nullptr);
 
   ShowWindow(hWnd, nCmdShow);
 
-  std::shared_ptr<Game> game = std::make_shared<Game>(hWnd, KEYBOARD);
+  auto game = std::make_shared<Game>(hWnd, KEYBOARD);
   
   MSG msg;
   game->run(&msg);
 
-  return msg.wParam;
+  return 0;
 }
 
 LRESULT CALLBACK MessageProcessor(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -52,6 +45,14 @@ LRESULT CALLBACK MessageProcessor(HWND hWnd, UINT message, WPARAM wParam, LPARAM
     case WM_KEYUP:
     {
       KEYBOARD->SetState(message, wParam);
+    } break;
+    case WM_MOUSEMOVE:
+    {
+      // Handle mouse movement if necessary
+    } break;
+    default:
+    {
+      // Handle other messages if necessary
     } break;
   }
   return DefWindowProc(hWnd, message, wParam, lParam);
