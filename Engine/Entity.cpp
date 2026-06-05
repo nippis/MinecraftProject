4a75710067629a42d9d141d21d58200c459d4fa4
Petteri Niemi <beteniemi@gmail.com>
2026-06-05 15:52:27 +0300

    Move getter implementations to header file

--- Engine/Entity.cpp
+++ Engine/Entity.cpp
@@ -4,15 +4,9 @@ using namespace DirectX;
 
 Entity::Entity(XMVECTOR location, float height, float width, float depth) :
   m_height(height), m_width(width), m_depth(depth), m_forward({ 0.0f, 0.0f, -1.0f }),
-  m_up({ 0.0f, 1.0f, 0.0f })
+  m_up({ 0.0f, 1.0f, 0.0f }), m_bBox({ 0.0f, 0.0f, 0.0f }, { width / 2, depth / 2, height / 2 })
 {
-  m_bBox = BoundingBox({0.0f, 0.0f, 0.0f }, {width / 2, depth / 2, height / 2});
   SetLocation(location);
-}
-
-const BoundingBox& Entity::GetBoundingBox() const
-{
-  return m_bBox;
 }
 
 void Entity::SetLocation(const XMVECTOR& location)
@@ -31,24 +25,9 @@ void Entity::SetUp(const XMVECTOR& up)
   XMStoreFloat3(&m_up, up);
 }
 
-XMVECTOR Entity::GetLocation() const
-{
-  return XMLoadFloat3(&m_location);
-}
-
-XMVECTOR Entity::GetDirection() const
-{
-  return XMLoadFloat3(&m_forward);
-}
-
 XMVECTOR Entity::GetForwardXZ() const
 {
   return XMVector3Normalize(XMVectorSet(m_forward.x, 0, m_forward.z, 0));
-}
-
-XMVECTOR Entity::GetUp() const
-{
-  return XMLoadFloat3(&m_up);
 }
 
 XMVECTOR Entity::GetRightXZ() const
--- Engine/Entity.h
+++ Engine/Entity.h
@@ -11,16 +11,21 @@ class Entity
 public:
   Entity(XMVECTOR location, float height, float width, float depth);
 
-  const BoundingBox& GetBoundingBox() const;
-
   virtual void SetLocation(const XMVECTOR& location);
   virtual void SetDirection(const XMVECTOR& forward);
   virtual void SetUp(const XMVECTOR& up);
 
-  virtual XMVECTOR GetLocation() const;
-  virtual XMVECTOR GetDirection() const;
+  [[nodiscard]]
+  const BoundingBox& GetBoundingBox() const { return m_bBox; }
+  [[nodiscard]]
+  virtual XMVECTOR GetLocation() const { return XMLoadFloat3(&m_location); }
+  [[nodiscard]]
+  virtual XMVECTOR GetDirection() const { return XMLoadFloat3(&m_forward); }
+  [[nodiscard]]
+  virtual XMVECTOR GetUp() const { return XMLoadFloat3(&m_up); }
+  [[nodiscard]]
   virtual XMVECTOR GetForwardXZ() const;
-  virtual XMVECTOR GetUp() const;
+  [[nodiscard]]
   virtual XMVECTOR GetRightXZ() const;
 
 protected:
