2242fa9695f7c52fb6426e5ed9c49330f260dc73
Petteri Niemi <beteniemi@gmail.com>
2026-06-25 13:14:43 +0300

    Refactor pixel shader code

--- Shaders/PixelShader.hlsl
+++ Shaders/PixelShader.hlsl
@@ -3,19 +3,20 @@ cbuffer LightCBuf
 	float4 lightPos;
 }
 
-static const float4 diffuseColor = { 1.0f, 1.0f, 1.0f, 0.0f };
-static const float diffuseIntensity = 1.0f;
-static const float attConst = 1.0f;
-static const float attLin = 1.0f;
-static const float attQuad = 1.0f;
+static const float diffuseIntensity = 0.7f;
+static const float ambientIntensity = 0.3f;
+
+float AngleCosNormalized(float4 vec1, float4 vec2)
+{
+    const float lightAngleCos = dot(normalize(vec1), normalize(vec2));
+    return (lightAngleCos + 1) / 2;
+}
 
 float4 main(float4 worldPos : TEXCOORD0, float4 n : TEXCOORD1, float4 position : SV_POSITION, float4 col : TEXCOORD2) : SV_TARGET
 {
-  const float4 vToL = lightPos - worldPos;
-  const float4 dirToL = normalize(vToL);
-  const float4 diffuse = diffuseColor * diffuseIntensity * col * dot(dirToL, n);
-  const float4 ambient = col * 0.4f + float4(0.1f, 0.1f, 0.1f, 0.0f);
-  
-  return saturate(diffuse + ambient);
+    const float diffuse = diffuseIntensity * AngleCosNormalized(lightPos - worldPos, n);
+    
+    const float totalIntensity = diffuse + ambientIntensity;
+    return saturate(col * totalIntensity);
 
 }
